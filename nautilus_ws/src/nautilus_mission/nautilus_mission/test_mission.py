a19efd85d67fc3855b0720638c977aafce337012
Felix Tremblay <tref1103@usherbrooke.ca>
2026-04-12 13:28:08 -0400

    NSFW76 Modified files from McGill tests

--- nautilus_ws/src/nautilus_mission/nautilus_mission/test_mission.py
+++ nautilus_ws/src/nautilus_mission/nautilus_mission/test_mission.py
@@ -32,7 +32,7 @@ class Master(Node):
 
         self.auv.StartReceiver()
 
-        self.validate_ekf()
+        # self.validate_ekf()
 
         self.mission()
 
@@ -47,12 +47,9 @@ class Master(Node):
         self.auv.ResetPosEstimate()
         self.auv.Arm() 
         self.auv.ChangeMode('GUIDED')
-        self.auv.GoToWaypointLocal(0, 0, 0.4, 0)
-        self.auv.GoToWaypointLocal(2, 0, 0.4, 0)
-
-
-
-        # self.auv.GoToWaypointLocal(0, 0, 0.8, pi/2)
+       
+        self.auv.GoToWaypointFRD(0, 0, 0.4, 0.82)
+        self.auv.GoToWaypointFRD(2, 0, 0.4, 0.82)
         # self.auv.GoToWaypointLocal(0, 17, 0.8, pi/2)
         # self.auv.GoToWaypointLocal(-1.5, 17, 0.8, pi)
         # self.auv.GoToWaypointLocal(0, 0, 0.8, (pi + pi/2))
@@ -66,13 +63,13 @@ class Master(Node):
         # self.auv.ChangeMode('POSHOLD')
         
         
-        while True:
-            yaw = 0
-            self.auv.SendVecCommand(0.5, yaw)
-            time.sleep(10)
-            yaw = pi
-            self.auv.SendVecCommand(0.5, yaw)
-            time.sleep(10)
+        # while True:
+        #     yaw = 0
+        #     self.auv.SendVecCommand(0, 0, 0.4, 0.5, yaw)
+        #     time.sleep(10)
+        #     yaw = pi
+        #     self.auv.SendVecCommand(0.5, yaw)
+        #     time.sleep(10)
 
         
 
