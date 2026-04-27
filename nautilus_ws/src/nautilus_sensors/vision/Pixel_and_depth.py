ba36733ec84752646f8df10b99b11fd9c84b8910
Rayvax2 <xlefebvre2003@gmail.com>
2026-04-27 17:26:28 -0400

    Add global depth for forward cam

--- nautilus_ws/src/nautilus_sensors/vision/Pixel_and_depth.py
+++ nautilus_ws/src/nautilus_sensors/vision/Pixel_and_depth.py
@@ -95,10 +95,10 @@ def find_angle(x_center, depth_mean, mode):
 
     if depth_mean is None or int(depth_mean) == 0:
         return None
-    # Coordonnée horizontale dans le repère caméra
+    #X pos
     X = (x_center - cx) * depth_mean / fx
 
-    # Yaw pour aligner l'objet avec le centre
+    #Yaw compare with middle cam
     yaw = np.arctan2(X, depth_mean)
     yaw_deg = np.degrees(yaw)
 
@@ -114,7 +114,7 @@ def global_median_forward_cam(depth_frame, mode):
 
     h, w = depth_frame.shape
 
-    # Définir la zone autour du centre
+    #Zone
     x1 = max(0, int(cx - cx/2))
     x2 = min(w, int(cx + cx/2))
     y1 = max(0, int(cy - cy/2))
@@ -122,7 +122,7 @@ def global_median_forward_cam(depth_frame, mode):
 
     patch = depth_frame[y1:y2, x1:x2]
 
-    # enlever les zéros
+    #Remove zeros median
     valid_values = patch[patch != 0]
 
     if valid_values.size == 0:
