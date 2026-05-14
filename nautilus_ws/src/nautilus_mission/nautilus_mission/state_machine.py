c26550fd14ba574820e9f8528cc8fe392eac65cf
Felix Tremblay <tref1103@usherbrooke.ca>
2026-05-13 21:11:41 -0400

    NSFW-135 Implemented transitions library into FSM

--- nautilus_ws/src/nautilus_mission/nautilus_mission/state_machine.py
+++ nautilus_ws/src/nautilus_mission/nautilus_mission/state_machine.py
@@ -1,23 +1,36 @@
 #!/usr/bin/env python3
 
-import argparse
 import time
-
 import rclpy
+
+from transitions import Machine
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray, Int8, Int16
 
 from nautilus_bringup.RobotState import RobotState
 from nautilus_bringup.ObjectID import ObjectID, GateLikeObjectID
-from nautilus_mission.state import State
-
 
 class StateMachine(Node):
     def __init__(self):
 
         super().__init__('state_machine')
 
-        self._state = State()
+        self.state_start_time = time.monotonic()
+
+        self.states = ['SEARCH','CENTER_GATE','APPROACH_GATE','TRAVERSE_GATE','APPROACH_MARKER','CIRCLE_MARKER','RETURN_GATE','APPROACH_ANY']
+
+        self.transitions = [
+            {'trigger': 'search_to_center',            'source': 'SEARCH',          'dest': 'CENTER_GATE',     'conditions': 'condition_search_to_center'},
+            {'trigger': 'center_to_approach',          'source': 'CENTER_GATE',     'dest': 'APPROACH_GATE',   'conditions': 'condition_center_to_approach'},
+            {'trigger': 'approach_to_traverse',        'source': 'APPROACH_GATE',   'dest': 'TRAVERSE_GATE',   'conditions': 'condition_approach_to_traverse'},
+            {'trigger': 'traverse_to_approach',        'source': 'TRAVERSE_GATE',   'dest': 'APPROACH_MARKER', 'conditions': 'condition_traverse_to_approach'},
+            {'trigger': 'approach_to_circle',          'source': 'APPROACH_MARKER', 'dest': 'CIRCLE_MARKER',   'conditions': 'condition_approach_to_circle'},
+            {'trigger': 'circle_to_return',            'source': 'CIRCLE_MARKER',   'dest': 'RETURN_GATE',     'conditions': 'condition_circle_to_return'},
+            {'trigger': 'return_gate_to_approach_any', 'source': 'RETURN_GATE',     'dest': 'APPROACH_ANY',    'conditions': 'condition_return_to_approach_any'},
+            {'trigger': 'approach_any_to_traverse',    'source': 'APPROACH_ANY',    'dest': 'TRAVERSE_GATE',   'conditions': 'condition_approach_any_to_traverse'}
+        ]
+
+        self.machine = Machine(model=self, states = self.states, initial = 'SEARCH', transitions = self.transitions, after_state_change = 'state_changed')
 
         self.target_gate_id = None
         self.target_object_id = None
@@ -40,114 +53,93 @@ class StateMachine(Node):
         self.depth_threshold_pub = self.create_publisher(Int16, '/yolo/depth_threshold', 10)
 
         # Timers
-        self.timer_state_machine = self.create_timer(1/20, self.state_machine)
-        self.timer_state_sender = self.create_timer(1/10, self.state_targets_sender)
-
-        # Initial state
-        self.state = RobotState.TRAVERSE_GATE
+        self.timer_state_machine = self.create_timer(1/20, self.state_machine_timer)
+        self.timer_state_sender = self.create_timer(1/20, self.state_targets_sender)
 
         self.target_object_id = [ObjectID.GATE_TOTAL]
         self.get_logger().info(f'Set target gate to : {self.target_object_id[0].name}')
 
-    def state_machine(self):
-        if self.state == RobotState.SEARCH:
-            if self.is_object_present():
-                self.state = RobotState.CENTER_GATE
+    # State machine loop ---------------------------------------------------------------------------
 
-        elif self.state == RobotState.CENTER_GATE:
-            if self.is_object_centered() and self.state.lifespan > 10.0:
-                self.state = RobotState.APPROACH_GATE
-                self.target_object_id = [ObjectID.GATE_TOTAL]
-                self.get_logger().info(f'Set target object to : {self.target_object_id[0].name}')
+    def state_machine_timer(self):
+        if self.state == 'SEARCH':
+            self.search_to_center()
 
-        elif self.state == RobotState.APPROACH_GATE:
-            if not self.is_object_present():
-                self.state = RobotState.TRAVERSE_GATE
-                msg = Int16()
-                msg.data = 15000
-                self.depth_threshold_pub.publish(msg)
+        elif self.state == 'CENTER_GATE':
+            self.center_to_approach()
 
-        elif self.state == RobotState.TRAVERSE_GATE:
+        elif self.state == 'APPROACH_GATE':
+            self.approach_to_traverse()
+
+        elif self.state == 'TRAVERSE_GATE':
             forward_msg = Int16()
             forward_msg.data = 1700
             self.forward_cmd_pub.publish(forward_msg)
+            self.traverse_to_circle()
 
-            if self.state.lifespan > 2.0:
-                self.target_object_id = [ObjectID.MARQUEUR]
-                self.get_logger().info(f'Set target object to : {self.target_object_id[0].name}')
+        elif self.state == 'CIRCLE_MARKER':
+            self.circle_to_return()
 
-                if self.is_target_approached(5000):
-                    msg = Int16()
-                    msg.data = 15000
-                    self.depth_threshold_pub.publish(msg)
-                    self.state = RobotState.CIRCLE_MARKER
-                    self.target_gate_id = GateLikeObjectID.SLALOM_SIDE_MID
+        elif self.state == 'RETURN_GATE':
+            self.return_gate_to_approach_any()
 
-        elif self.state == RobotState.CIRCLE_MARKER:
-            if self.state.lifespan > 10.0 and self.is_gate_present() and self.mean_depth_forward_cam > 8200:
-                msg = Int16()
-                msg.data = 15000
-                self.depth_threshold_pub.publish(msg)
-                self.state = RobotState.RETURN_GATE
+        elif self.state == 'APPROACH_ANY':
+            self.approach_any_to_traverse()
 
-        elif self.state == RobotState.RETURN_GATE:
-            forward_msg = Int16()
-            forward_msg.data = 1700
-            self.forward_cmd_pub.publish(forward_msg)
+    # On enter or exit actions ----------------------------------------------------------------------
 
-            if self.state.lifespan > 5.0:
-                self.target_object_id = [ObjectID.GATE_LEG, ObjectID.GATE_TOTAL, ObjectID.LUMIERE]
-                self.get_logger().info(f'Set target object to : {self.target_object_id[0].name}')
-                self.state = RobotState.APPROACH_ANY
+    def on_enter_CENTER_GATE(self):
+        msg = Int16()
+        msg.data = 7000
+        self.depth_threshold_pub.publish(msg)
 
-        elif self.state == RobotState.APPROACH_ANY:
-            if self.is_target_approached(2000):
-                msg = Int16()
-                msg.data = 5000
-                self.depth_threshold_pub.publish(msg)
-                self.state = RobotState.CENTER_GATE
+    def on_enter_APPROACH_GATE(self):
+        self.target_object_id = [ObjectID.GATE_TOTAL]
+        self.get_logger().info(f'Set target object to : {self.target_object_id[0].name}')
 
-    def state_targets_sender(self):
-        msg = Int8()
+    def on_enter_TRAVERSE_GATE(self):
+        msg = Int16()
+        msg.data = 15000
+        self.depth_threshold_pub.publish(msg)
 
-        if self.state._state is not None:
-            msg.data = self.state.value
-            self.state_pub.publish(msg)
+    def on_enter_APPROACH_MARKER(self):
+        self.target_object_id = [ObjectID.MARQUEUR]
+        self.get_logger().info(f'Set target object to : {self.target_object_id[0].name}')
 
-        if self.target_gate_id is not None:
-            msg.data = self.target_gate_id
-            self.target_gate_pub.publish(msg)
+    def on_enter_CIRCLE_MARKER(self):
+        self.target_gate_id = GateLikeObjectID.SLALOM_SIDE_MID
 
-        target_object = self.get_target_object()
+    def on_enter_APPROACH_ANY(self):
+        self.target_object_id = [ObjectID.GATE_LEG, ObjectID.GATE_TOTAL, ObjectID.LUMIERE]
+        self.get_logger().info(f'Set target object to : {self.target_object_id[0].name}, {self.target_object_id[1].name}, {self.target_object_id[2].name}')
 
-        if target_object is not None:
-            msg.data = int(target_object[0])
-            self.target_object_pub.publish(msg)
+    # State changement trigger conditions ------------------------------------------------------------
     
-    def mean_depth_callback(self, msg):
-            self.mean_depth_forward_cam = msg.data
+    def condition_search_to_center(self):
+        return self.is_object_present()
+    
+    def condition_center_to_approach(self):
+        return self.is_object_centered() and self.state_lifespan > 10.0
+    
+    def condition_approach_to_traverse(self):
+        return not self.is_object_present()
+    
+    def condition_traverse_to_approach(self):
+        return self.state_lifespan > 3.0
 
-    def obj_detection_callback(self, msg):
-        data = msg.data
-        self.objects = [data[i:i+3] for i in range(0, len(data), 3)]
-        
-    def gate_detection_callback(self, msg):
-        data = msg.data
-        self.gate_objects = [data[i:i+3] for i in range(0, len(data), 3)]
+    def condition_approach_to_circle(self):
+        return self.is_target_approached(5000)
+    
+    def condition_circle_to_return(self):
+        return (self.is_gate_present() and self.mean_depth_forward_cam > 8200)
+    
+    def condition_return_to_approach_any(self):
+        return self.state_lifespan > 5.0  
+    
+    def condition_approach_any_to_traverse(self):
+        return self.is_target_approached(2000)
 
-    def get_target_object(self):
-        if self.target_object_id is not None and self.objects is not None:
-            target_object = next((o for o in self.objects if ObjectID(o[0]) in self.target_object_id),None)
-            return target_object
-        else:
-            return None
-
-    def get_target_gate(self):
-        if self.target_gate_id is not None and self.gate_objects is not None:
-            target_gate = next((o for o in self.gate_objects if GateLikeObjectID(o[0]) == self.target_gate_id),None)
-            return target_gate
-        else:
-            return None
+    # Detection methods -------------------------------------------------------------------------------
 
     def is_gate_present(self):
         target_gate = self.get_target_gate()
@@ -186,15 +178,62 @@ class StateMachine(Node):
                 self.get_logger().info(f"Target object : {self.target_object_id[0].name} is in range!")
                 return True
         return False
+    
+    # Publisher loops or subscribers callbacks --------------------------------------------------------
+
+    def state_targets_sender(self):
+        msg = Int8()
+
+        if self.state is not None:
+            msg.data = RobotState[self.state].value
+            self.state_pub.publish(msg)
+
+        if self.target_gate_id is not None:
+            msg.data = self.target_gate_id
+            self.target_gate_pub.publish(msg)
+
+        target_object = self.get_target_object()
+
+        if target_object is not None:
+            msg.data = int(target_object[0])
+            self.target_object_pub.publish(msg)
+
+    def obj_detection_callback(self, msg):
+        data = msg.data
+        self.objects = [data[i:i+3] for i in range(0, len(data), 3)]
+        
+    def gate_detection_callback(self, msg):
+        data = msg.data
+        self.gate_objects = [data[i:i+3] for i in range(0, len(data), 3)]
+
+    def mean_depth_callback(self, msg):
+            self.mean_depth_forward_cam = msg.data
+
+    # YOLO_PIPELINE parser for target_id ------------------------------------------------------------
+
+    def get_target_object(self):
+        if self.target_object_id is not None and self.objects is not None:
+            target_object = next((o for o in self.objects if ObjectID(o[0]) in self.target_object_id),None)
+            return target_object
+        else:
+            return None
+
+    def get_target_gate(self):
+        if self.target_gate_id is not None and self.gate_objects is not None:
+            target_gate = next((o for o in self.gate_objects if GateLikeObjectID(o[0]) == self.target_gate_id),None)
+            return target_gate
+        else:
+            return None
+    
+    # Necessary for state lifespan --------------------------------------------------------------------
+
+    def state_changed(self):
+        self.state_start_time = time.monotonic()
+        self.get_logger().info(f"Entered state {self.state}")
 
     @property
-    def state(self):
-        return self._state
-
-    @state.setter
-    def state(self, new_state):
-        self._state.set(new_state)
-        self.get_logger().info(f'Set state to : {new_state.name}')
+    def state_lifespan(self):
+        return time.monotonic() - self.state_start_time
 
 
 def main(args=None):
