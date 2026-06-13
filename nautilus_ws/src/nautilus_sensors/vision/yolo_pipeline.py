f044b23521184a6fcd3c9cdc3b2030b241131aec
Nautilus_tour <eaimez03@gmail.com>
2026-06-12 20:24:30 -0400

    Modified the object id and yolo model for testing with new model

--- nautilus_ws/src/nautilus_sensors/vision/yolo_pipeline.py
+++ nautilus_ws/src/nautilus_sensors/vision/yolo_pipeline.py
@@ -53,7 +53,7 @@ class YoloNode(Node):
         # -------- MODE --------
         if args.sim:
             self.mode = 'sim'
-            model_path = os.path.expanduser('~/NautilusSW/nautilus_ws/src/nautilus_sensors/vision/yolo_models/bbox_sim_640_24_mai.pt')
+            model_path = os.path.expanduser('~/NautilusSW/nautilus_ws/src/nautilus_sensors/vision/yolo_models/bbox_sim_640_6_juin.pt')
         else:
             self.mode = 'real'
             model_path = os.path.expanduser('~/NautilusSW/nautilus_ws/src/nautilus_sensors/vision/yolo_models/bbox_competition_23_mai.pt')
