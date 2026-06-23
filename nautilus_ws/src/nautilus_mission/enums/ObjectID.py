4a47d0cba8d98f297676d389b16ce166acb0755e
Rayvax2 <xlefebvre2003@gmail.com>
2026-06-22 23:14:32 -0400

    Add angle for torpedo

--- nautilus_ws/src/nautilus_mission/enums/ObjectID.py
+++ nautilus_ws/src/nautilus_mission/enums/ObjectID.py
@@ -17,10 +17,12 @@ class ObjectID(IntEnum):
     HAMMER = 12
     TORPEDO = 13
     GATE = 14
-    SLALOM_LEFT = 15
-    SLALOM_RIGHT = 16
+    FIRE_TRUCK = 15
+    AMBULANCE = 16
     GATE_LEFT_MID = 20
     GATE_MID_RIGHT = 21
     SLALOM_LEFT_MID = 22
     SLALOM_MID_RIGHT = 23
+    SLALOM_LEFT = 30
+    SLALOM_RIGHT = 31
     
