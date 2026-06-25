5e3a60f68e10be79c51561b91642e21b022eff77
Felix Tremblay <tref1103@usherbrooke.ca>
2026-06-24 22:04:02 -0400

    NSFW-149 updated servo enum positions for dropper

--- nautilus_ws/src/nautilus_mission/enums/ServoEnum.py
+++ nautilus_ws/src/nautilus_mission/enums/ServoEnum.py
@@ -3,9 +3,9 @@ from enum import IntEnum
 class ServoEnum(IntEnum):
     TORPEDO_ID = 11
     DROPPER_ID = 12
-    DROPPER_INIT_PWM = 1500
-    DROPPER_1_PWM = 1100
-    DROPPER_2_PWM = 1900
+    DROPPER_INIT_PWM = 2200
+    DROPPER_1_PWM = 1500
+    DROPPER_2_PWM = 800
     TORPEDO_INIT_PWM = 1250
     TORPEDO_L_PWM = 800
     TORPEDO_R_PWM = 2100
