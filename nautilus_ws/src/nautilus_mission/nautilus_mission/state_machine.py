a3f7a6e2cedf6bacab02768af786ca8781873bc3
Felix Tremblay <tref1103@usherbrooke.ca>
2026-06-27 11:36:19 -0400

    added piscine modifications

--- nautilus_ws/src/nautilus_mission/nautilus_mission/state_machine.py
+++ nautilus_ws/src/nautilus_mission/nautilus_mission/state_machine.py
@@ -187,7 +187,7 @@ class StateMachine:
                 # self.node.fire_torpedo()
                 self.node.get_logger().info('Launching torpedo no 1!')
                 self.node.publish_servo_cmd(ServoEnum.TORPEDO_ID, ServoEnum.TORPEDO_R_PWM)  
-                self.node.publish_servo_cmd(ServoEnum.TORPEDO_ID, ServoEnum.TORPEDO_INIT_PWM)
+                #self.node.publish_servo_cmd(ServoEnum.TORPEDO_ID, ServoEnum.TORPEDO_INIT_PWM)
         if self.current_objective.action.type == ActionType.FORWARD:
             error_ekf_fwd_position = self.current_objective.action.forward_distance_m - self.forward_position
             self.node.publish_forward_ekf_error(error_ekf_fwd_position)
