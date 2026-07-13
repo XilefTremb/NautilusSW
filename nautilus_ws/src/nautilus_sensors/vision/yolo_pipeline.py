94b1806ea228b00a7acdef9b14e2390339ccfb35
Felix Tremblay <tref1103@usherbrooke.ca>
2026-07-12 20:17:24 -0400

    new model

--- nautilus_ws/src/nautilus_sensors/vision/yolo_pipeline.py
+++ nautilus_ws/src/nautilus_sensors/vision/yolo_pipeline.py
@@ -64,7 +64,7 @@ class YoloNode(Node):
             model_path = os.path.expanduser('~/NautilusSW/nautilus_ws/src/nautilus_sensors/vision/yolo_models/bbox_sim_640_16_juin.pt')
         else:
             self.mode = 'real'
-            model_path = os.path.expanduser('~/NautilusSW/nautilus_ws/src/nautilus_sensors/vision/yolo_models/bbox_4juillet_competition.pt')
+            model_path = os.path.expanduser('~/NautilusSW/nautilus_ws/src/nautilus_sensors/vision/yolo_models/bbox_12juillet_competition.pt')
 
         # -------- MODEL TYPE --------
         self.type_yolo = 'obb' if args.obb else 'bbox'
